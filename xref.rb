1641e101962a84a2f29ea47e56119312af902ad0
1AdvaithSuresh <53131970+1AdvaithSuresh@users.noreply.github.com>
2019-11-08 19:10:50 -0500

    Update xref.rb

--- xref.rb
+++ xref.rb
@@ -3,7 +3,9 @@ require_relative "main.rb"
 
 dwarf_dump= `llvm-dwarfdump --debug-line #{ARGV[0]}`
 assembly = `objdump --disassemble #{ARGV[0]}` 
-lookup_table = readdwarf(dwarf_dump)
+lookup_table_master = readdwarf(dwarf_dump)
+lookup_table = lookup_table_master[0]
+unused_table = lookup_table_master[1]
 puts "Lookup Table"
 puts lookup_table
 
@@ -46,34 +48,67 @@ assembly.each_line do |line|
   if !addr.nil? == true && addr.match(/:/)
     addr = addr.gsub(/:/, '')
   lookup_table.each do |key, value|
-    #if key.e previous_key
-    #else
-      #assembly_table += "<tr><td>~</td><td>#{key}</td></tr>"
-    #end
-   # previous_key = key
     asmly_addr = value.keys
     asmly_addr.each do |item|
+      puts "LOOK OVER HERE"
+      current_source_line = value[item][0].to_i #current source line being checked
       if item.kind_of?(Array)
         if item[0].match(/#{addr}/)
+          if item[0].match(/&ltmain&gt/)
+            assembly_table += "<tr><td><a id=\"target\"></a>"
+          else
+            assembly_table += "<tr><td>"
+          end
           #puts "#{tmp}                               #{'c:'+value[item]}"
           if key.eql? previous_key
           else
-           assembly_table += "<tr><td>~</td><td>#{key}</td></tr>"
+           assembly_table += "<tr><td>~</td><td>#{key}</td></tr><tr><td>"
           end
           previous_key = key
-          assembly_table = assembly_table +"<tr><td>#{linenum_string}#{tmp}</td><td>#{value[item]}</td></tr>"
+          assembly_table = assembly_table +"#{linenum_string}#{tmp}</td><td>#{value[item]}</td></tr>" #add new row to table with "assembly => source"
           f = true
+          #check if next source code lines do not have corresponding assembly lines
+          more_lines = true #remains true as long as the next source line does not have corresponding assembly lines
+          while more_lines
+            current_source_line= current_source_line +1
+            if unused_table.key?(key)
+              source_lines= unused_table[key]
+              if source_lines.key?(current_source_line)
+                 assembly_table += "<tr><td>~</td><td>#{source_lines[current_source_line]}</td></tr><tr><td>"#create new table row to display unused source code
+              else
+                 more_lines = false
+              end
+            end
+          end #end of while loop
         end
       else
         if item.match(/#{addr}/)
+          if item.match(/&ltmain&gt/)
+            assembly_table += "<tr><td><a id=\"target\"></a>"
+          else
+            assembly_table += "<tr><td>"
+          end
           #puts "#{tmp}                               #{'c:'+value[item]}"
           if key.eql? previous_key
           else
-            assembly_table += "<tr><td>~</td><td>#{key}</td></tr>"
+            assembly_table += "<tr><td>~</td><td>#{key}</td></tr><tr><td>"
           end
           previous_key = key
-          assembly_table = assembly_table +"<tr><td>#{linenum_string}#{tmp}</td><td>#{value[item]}</td></tr>"
+          assembly_table = assembly_table +"#{linenum_string}#{tmp}</td><td>#{value[item]}</td></tr>"
           f = true
+          #check if next source code lines do not have corresponding assembly lines
+          more_lines = true #remains true as long as the next source line does not have corresponding assembly lines
+          while more_lines
+            current_source_line = current_source_line + 1
+            if unused_table.key?(key) 
+              source_lines= unused_table[key]
+              if source_lines.key?(current_source_line)
+                 assembly_table += "<tr><td>~</td><td>#{source_lines[current_source_line]}</td></tr><tr><td>"#create new table row to display unused source code
+              else
+                 more_lines = false
+              end
+            end
+          end #end of while loop
         end
       end #end of if/else
     end #end of item loop
@@ -81,12 +116,24 @@ assembly.each_line do |line|
   end
  if f == false
    puts line
-   assembly_table = assembly_table +"<tr><td>" +linenum_string+ line + "</td></tr>"
+   if line.match(/&ltmain&gt/)
+            assembly_table += "<tr><td><a id=\"target\"></a>"
+   else
+      assembly_table += "<tr><td>"
+   end
+   assembly_table = assembly_table  +linenum_string+ line + "</td></tr>"
  end
  f = false
 end
-result = result + assembly_table + "</div>
+result = result + assembly_table + "<a href=\"#target\">Link</a>
+</div>
         </body>
     </html>"
 puts "HTML CODE GENERATED:"
 puts result
+
+#Create directory and write html files
+Dir.mkdir('html') unless Dir.exist?('html') #creates directory if it does not already exist
+File.open('html/output.html','w') do |file|
+  file.write(result) 
+end
